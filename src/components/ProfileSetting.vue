08536a32c99079ebf8d18ff69571eb11d674b818
Akram Fawzi Mihoubi <mihhakram@gmail.com>
2025-03-19 16:08:23 +0100

    ProfileSetting Component

--- src/components/ProfileSetting.vue
+++ src/components/ProfileSetting.vue
@@ -1,53 +1,82 @@
 <template>
     <div class="profile-setting">
       <SplitButton 
-        label="Mohamed Benhassir" 
-        :model="items" 
-        class="p-button-rounded p-button-secondary">
-        <template #buttonicon>
-          <img src="@/assets/user.svg" alt="User Icon" class="user-icon" />
-        </template>
+        :model="items"
+        text 
+        severity="contrast"
+        class="p-button-rounded p-button-secondary"
+      >
+        <span class="profile-items">
+          <img  src="@/assets/user.svg" />
+          <div class="profile-info">
+            <span class="name">{{ user.name }}</span>
+            <span class="email">{{ user.email }}</span>
+          </div>  
+        </span>
       </SplitButton>
     </div>
   </template>
   
   <script setup>
-  import { ref } from 'vue';
+  import { defineProps } from 'vue';
   import SplitButton from 'primevue/splitbutton';
-
   
-  const items = [
-    {
-      label: 'Business settings',
-      icon: 'pi pi-briefcase',
-      command: () => { /* your command */ },
+  defineProps({
+    user: { 
+      type: Object, 
+      required: true,
+      default: () => ({
+        name: 'Guest User',
+        email: 'guest@example.com',
+        image: '/default-user.svg'
+      })
     },
-    {
-      label: 'Account settings',
-      icon: 'pi pi-cog',
-      command: () => { /* your command */ },
-    },
-    {
-      label: 'Our products',
-      icon: 'pi pi-box',
-      command: () => { /* your command */ },
-    },
-    {
-      label: 'Log out',
-      icon: 'pi pi-sign-out',
-      command: () => { /* your command */ },
-    }
-    ];
+    items: { type: Array, required: true }
+  });
   </script>
   
   <style scoped>
+
+  .p-splitbutton {
+    width: 300px;
+    justify-content: space-between;
+  }
   .profile-setting {
     display: flex;
     align-items: center;
   }
-  
-  .user-icon {
-    width: 24px;
-    height: 24px;
+  .profile-items {
+    display: flex;
+    align-items: center;
   }
+  .profile-items img {
+    width: 2.7rem;
+    margin-right: 0.7rem;
+  }
+  .p-button-secondary {
+    background: var(--p-button-primary-color);
+    border: 1px solid var(--p-button-secondary-border-color);
+    border-radius: 20px;
+  }
+  .profile-info {
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+  }
+  .name {
+    font-size: 0.9rem;
+    font-weight: 700;
+  }
+  .email {
+    font-size: 0.75rem;
+    color: #BEBCBC;
+  }
+  
+:deep(.p-splitbutton .p-splitbutton-button:not(:disabled):hover),
+:deep(.p-splitbutton .p-splitbutton-dropdown:not(:disabled):hover) {
+  background-color: white !important;
+  border-color: white !important;
+}
+
   </style>
+  
