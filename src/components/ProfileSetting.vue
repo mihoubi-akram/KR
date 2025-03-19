ddefaea2d174fd507e8186718f9935c86bd6df14
Akram Fawzi Mihoubi <mihhakram@gmail.com>
2025-03-19 16:07:26 +0100

    wip profile setting

---
+++ src/components/ProfileSetting.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="profile-setting">
+      <SplitButton 
+        label="Mohamed Benhassir" 
+        :model="items" 
+        class="p-button-rounded p-button-secondary">
+        <template #buttonicon>
+          <img src="@/assets/user.svg" alt="User Icon" class="user-icon" />
+        </template>
+      </SplitButton>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  import SplitButton from 'primevue/splitbutton';
+
+  
+  const items = [
+    {
+      label: 'Business settings',
+      icon: 'pi pi-briefcase',
+      command: () => { /* your command */ },
+    },
+    {
+      label: 'Account settings',
+      icon: 'pi pi-cog',
+      command: () => { /* your command */ },
+    },
+    {
+      label: 'Our products',
+      icon: 'pi pi-box',
+      command: () => { /* your command */ },
+    },
+    {
+      label: 'Log out',
+      icon: 'pi pi-sign-out',
+      command: () => { /* your command */ },
+    }
+    ];
+  </script>
+  
+  <style scoped>
+  .profile-setting {
+    display: flex;
+    align-items: center;
+  }
+  
+  .user-icon {
+    width: 24px;
+    height: 24px;
+  }
+  </style>
