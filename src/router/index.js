59839022c75493f3ea936f033bf2c64799cc1a36
Akram Fawzi Mihoubi <mihhakram@gmail.com>
2025-03-27 03:10:26 +0100

    Refactor router to use auth store for authentication checks and redirects

--- src/router/index.js
+++ src/router/index.js
@@ -7,12 +7,44 @@ import ResetPasswordView from '@/views/ResetPasswordView.vue';
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
-  { path: '/', redirect: () => (localStorage.getItem('token') ? '/home' : '/login') },
-  { path: '/login', name: 'login', component: LoginView, meta: { guest: true } },
-  { path: '/register', name: 'register', component: RegisterView, meta: { guest: true } },
-  { path: '/forgot-password',name: 'forgot-password',component: ForgotPasswordView,meta: { guest: true }},
-  { path: '/reset-password/:token',name: 'reset-password',props:true, component: ResetPasswordView,meta: { guest: true }},
-  { path: '/home', name: 'home', component: HomeView, meta: { requiresAuth: true } },
+  { 
+    path: '/', 
+    redirect: () => {
+      const authStore = useAuthStore();
+      return authStore.token ? '/home' : '/login';
+    } 
+  },
+  { 
+    path: '/login', 
+    name: 'login', 
+    component: LoginView, 
+    meta: { guest: true } 
+  },
+  { 
+    path: '/register', 
+    name: 'register', 
+    component: RegisterView, 
+    meta: { guest: true } 
+  },
+  { 
+    path: '/forgot-password',
+    name: 'forgot-password',
+    component: ForgotPasswordView,
+    meta: { guest: true }
+  },
+  { 
+    path: '/reset-password/:token',
+    name: 'reset-password',
+    props: true, 
+    component: ResetPasswordView,
+    meta: { guest: true }
+  },
+  { 
+    path: '/home', 
+    name: 'home', 
+    component: HomeView, 
+    meta: { requiresAuth: true } 
+  },
   { 
     path: '/logout', 
     name: 'logout', 
@@ -30,14 +62,15 @@ const router = createRouter({
 })
 
 router.beforeEach((to, from, next) => {
-  const isAuthenticated = !!localStorage.getItem('token')
+  const authStore = useAuthStore();
+  const isAuthenticated = !!authStore.token;
 
   if (to.meta.requiresAuth && !isAuthenticated) {
-    next('/login') 
+    next('/login');
   } else if (to.meta.guest && isAuthenticated) {
-    next('/home') 
+    next('/home');
   } else {
-    next()
+    next();
   }
 })
 
